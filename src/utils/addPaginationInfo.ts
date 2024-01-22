b4340b0197606cb5bdb3ce95cd8d241adc0097a4
Carmine Conversano <carmine.conversano@overzoom.it>
2024-01-22 02:33:44 +0100

    fix pagination

--- src/utils/addPaginationInfo.ts
+++ src/utils/addPaginationInfo.ts
@@ -1,7 +1,7 @@
-import {SuccessResponse} from "../models/shared/SuccessResponse";
+import {SuccessResponse} from "../models";
 
 export function addPaginationInfo(response: SuccessResponse<any>) {
-    response.pages = Number(response.headers["x-wp-total-pages"]);
-    response.total = Number(response.headers["x-wp-total"]);
+    response.pages = Number(response.headers["x-wp-total-pages"] || '0');
+    response.total = Number(response.headers["x-wp-total"] || '0');
     return response;
 }

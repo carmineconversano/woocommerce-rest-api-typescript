20da260a72341f1bb89d4350162b7917178d8346
Carmine Conversano <carmine.conversano@overzoom.it>
2024-01-29 18:29:39 +0100

    add service

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './models/index';
 export * from './services/order/order-woo.service'
 export * from './services/product/product-woo.service'
 export * from './services/customer/customer-woo.service'
+export * from './services/product-variations/product-variations-woo.service'
